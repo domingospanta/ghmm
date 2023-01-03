dbdd9878bb08fea836258ba7af46faa91c885509
dpantasap <domingos.panta.junior@sap.com>
2023-01-03 09:53:30 +0000

    First commit.

---
+++ src/main/java/pt/feup/ghmm/GhmmApplication.java
@@ -0,0 +1,13 @@
+package pt.feup.ghmm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GhmmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GhmmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pt/feup/ghmm/GhmmApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.feup.ghmm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GhmmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
