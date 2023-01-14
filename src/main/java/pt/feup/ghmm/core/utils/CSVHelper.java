0912743fb7a7f534b72c12b8a80ba38dc10a6717
domingospanta <juniordfpj@gmail.com>
2023-01-14 13:23:43 +0000

    add: unit tests for class CSVHelper.

--- src/main/java/pt/feup/ghmm/core/utils/CSVHelper.java
+++ src/main/java/pt/feup/ghmm/core/utils/CSVHelper.java
@@ -8,9 +8,11 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
+import io.micrometer.common.util.StringUtils;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
+import org.apache.logging.log4j.util.Strings;
 import org.springframework.web.multipart.MultipartFile;
 import pt.feup.ghmm.metrics.models.RepoExample;
 
@@ -19,7 +21,7 @@ public class CSVHelper {
 
     public static boolean hasCSVFormat(MultipartFile file) {
 
-        if (!TYPE.equals(file.getContentType())) {
+        if (file == null || !TYPE.equals(file.getContentType())) {
             return false;
         }
 
@@ -27,6 +29,7 @@ public class CSVHelper {
     }
 
     public static List<RepoExample> csvToRepoExamples(InputStream is) {
+        if(is == null) return new ArrayList<>();
         try (BufferedReader fileReader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
              CSVParser csvParser = new CSVParser(fileReader,
                      CSVFormat.DEFAULT.withFirstRecordAsHeader().withIgnoreHeaderCase().withTrim())) {
@@ -41,14 +44,43 @@ public class CSVHelper {
                         .url(csvRecord.get("url"))
                         .microservice(Boolean.parseBoolean(csvRecord.get("microservice")))
                         .build();
-
+                boolean isUrlValid = isUrlValid(repoExample.getUrl());
+                if(isUrlValid){
+                    if(StringUtils.isEmpty(repoExample.getOwner())){
+                        repoExample.setOwner(getOwnerFromUrl(repoExample.getUrl()));
+                    }
+                    if(StringUtils.isEmpty(repoExample.getName())){
+                        repoExample.setOwner(getNameFromUrl(repoExample.getUrl()));
+                    }
+                } else{
+                    repoExample.setUrl(repoExample.getUrl() + " invalid URL!");
+                    repoExample.setOwner(null);
+                    repoExample.setName(null);
+                }
                 repos.add(repoExample);
             }
-
             return repos;
         } catch (IOException e) {
             throw new RuntimeException("fail to parse CSV file: " + e.getMessage());
         }
     }
+
+    public static boolean isUrlValid(String url) {
+        return StringUtils.isNotEmpty(url) && url.contains("github") && url.split("/").length == 5;
+    }
+
+    public static String getOwnerFromUrl(String url) {
+        if (StringUtils.isEmpty(url)) return Strings.EMPTY;
+        String[] urlArray = url.split("/");
+        return urlArray[3];
+    }
+
+    public static String getNameFromUrl(String url) {
+        if (StringUtils.isEmpty(url)) return Strings.EMPTY;
+        String[] urlArray = url.split("/");
+        return urlArray[4];
+    }
+
+
 }
 
---
+++ src/test/java/pt/feup/ghmm/core/utils/CSVHelperTest.java
@@ -0,0 +1,83 @@
+package pt.feup.ghmm.core.utils;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.mock.web.MockMultipartFile;
+import pt.feup.ghmm.metrics.models.RepoExample;
+
+import java.io.IOException;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CSVHelperTest {
+
+    @Test
+    void hasCSVFormat() {
+
+        assertFalse(CSVHelper.hasCSVFormat(null));
+
+        MockMultipartFile file = new MockMultipartFile("test.json", "", "", "".getBytes());
+        assertFalse(CSVHelper.hasCSVFormat(file));
+
+        file = new MockMultipartFile("test.csv", "", "text/csv", "".getBytes());
+        assertTrue(CSVHelper.hasCSVFormat(file));
+    }
+
+    @Test
+    void csvToRepoExamples() throws IOException {
+
+        List<RepoExample> repoExamples = CSVHelper.csvToRepoExamples(null);
+        assertEquals( 0, repoExamples.size());
+
+        MockMultipartFile file = new MockMultipartFile("test.json", "", "text/csv", ("" +
+                "owner,name,url,microservice\n" +
+                ",instana.robot.shop,https://github.com/instana/robot-shop,TRUE").getBytes());
+
+        repoExamples = CSVHelper.csvToRepoExamples(file.getInputStream());
+        assertEquals( 1, repoExamples.size());
+
+        RepoExample repoExample = repoExamples.get(0);
+        assertEquals( "instana", repoExample.getOwner());
+        assertEquals( "instana.robot.shop", repoExample.getName());
+        assertEquals( "https://github.com/instana/robot-shop", repoExample.getUrl());
+        assertTrue( repoExample.isMicroservice());
+    }
+
+    @Test
+    void isUrlValid() {
+        String url = "";
+        boolean isGitHubUrlValid = CSVHelper.isUrlValid(url);
+        assertFalse(isGitHubUrlValid);
+
+        url = "https://test.com/instana/robot-shop";
+        isGitHubUrlValid = CSVHelper.isUrlValid(url);
+
+        assertFalse(isGitHubUrlValid);
+
+        url = "https://github.com/instana/robot-shop";
+        isGitHubUrlValid = CSVHelper.isUrlValid(url);
+        assertTrue(isGitHubUrlValid);
+    }
+
+    @Test
+    void getOwnerFromUrl() {
+        String url = "";
+        String ownerFromUrl = CSVHelper.getOwnerFromUrl(url);
+        assertEquals("", ownerFromUrl);
+
+        url = "https://github.com/instana/robot-shop";
+        ownerFromUrl = CSVHelper.getOwnerFromUrl(url);
+        assertEquals("instana", ownerFromUrl);
+    }
+
+    @Test
+    void getNameFromUrl() {
+        String url = "";
+        String nameFromUrl = CSVHelper.getNameFromUrl(url);
+        assertEquals("", nameFromUrl);
+
+        url = "https://github.com/instana/robot-shop";
+        nameFromUrl = CSVHelper.getNameFromUrl(url);
+        assertEquals("robot-shop", nameFromUrl);
+    }
+}
